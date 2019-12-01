5ced7b57deab2de0d3488f7fad2313b849857d2c
willroymyles <willroymyles@yahoo.com>
2019-12-01 13:46:21 -0800

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
-var moment = require('moment');
 
 
 var app = express();
 app.use(express.static('./public/'));
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-//authentication
-//mot to another file soon
 
 
 
@@ -28,9 +24,6 @@ function start() {
     app.listen(port);
     console.log("listening on port " + port);
 
-    var db = require('./controllers/databaseController');
-    db.db(app);
-    var pc = require('./controllers/postController')(app, db);
-    var gc = require('./controllers/getController')(app, db);
-    var putCon = require('./controllers/putController')(app, db);
+    var gc = require('./controllers/getController')(app);
+
 }
